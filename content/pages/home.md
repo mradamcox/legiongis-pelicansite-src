def779e3b6f8472a791bdc737b4088057777982b
Adam Cox <mr.adamcox@gmail.com>
2016-02-25 11:50:10 -0600

    initial commit

---
+++ content/pages/home.md
@@ -0,0 +1,10 @@
+Title: Home
+urls: basemap1; basemap2
+template: index
+save_as: index.html
+url: index.html
+sortorder: 1
+
+&nbsp;&nbsp;&nbsp;&nbsp;Legion GIS, LLC is a small consulting firm specializing in Geographic Information Systems.  We have experience in a wide variety of applications, from cultural heritage to conservation science to hazard mitigation.  We see GIS as an exciting way to view and explore the world, but one that is largely unknown to everyday folks.  We want to change that!  For more information on what we can do for your organization, please check out our services.
+
+&nbsp;&nbsp;&nbsp;&nbsp;We are also very excited to have a blog, and plan to fill it up with all kinds of interesting information... from technical guides to commentary to details about how we started this business.  Truly, we hope to share everything we can.
---
+++ pelicanconf.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+from datetime import date
+
+AUTHOR = u'LegionGIS'
+SITENAME = u'Legion GIS, LLC'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = u'en'
+PUBLISH_DATE = date.today().strftime("%b %d, %Y")
+
+SUMMARY_MAX_LENGTH = 25
+
+THEME = "theme"
+
+SERVICES_PAGES = (
+    ('Arches', "{}/pages/services/arches.html".format(SITEURL)),
+    ('Cartography', "{}/pages/services/cartography.html".format(SITEURL)),
+    ('Education', "{}/pages/services/education.html".format(SITEURL)),
+    )
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# links of interest
+LINKS = (
+    ('Quantum GIS', 'http://qgis.org/'),
+    ('ArcGIS', 'http://esri.com/'),
+    ('Arches', 'http://archesproject.org/'),
+    ('GIMP', 'http://gimp.org'),
+    ('GeoServer', 'http://geoserver.org'),
+    ('Inkscape', 'http://inkscape.org'),
+    ('Python', 'http://python.org/'),
+    ('Amazon Web Services', 'https://aws.amazon.com/'),
+    ('Pelican', 'http://getpelican.com/'),
+    ('TileMill', 'https://www.mapbox.com/tilemill/'),
+    )
+
+MAP_GALLERY = (
+    
+    ("{}/theme/img/map_gallery/Gunthorp Farm.png".format(SITEURL), 'Food Producer Cluster map for Crossroads Resource Center'),
+    ("{}/theme/img/map_gallery/CRC_ME_elevation.png".format(SITEURL), 'Phyiographic map for Crossroads Resource Center'),
+    ("{}/theme/img/map_gallery/FullCounty_physiographic.png".format(SITEURL), 'Hazard Mitigation map for OCR West'),
+    ("{}/theme/img/map_gallery/SanAugustineDetail.jpg".format(SITEURL), 'Hazard Mitigation map for MPTX'),
+    ("{}/theme/img/map_gallery/viroqua_to_sidieFINAL (Morea) reduce.png".format(SITEURL), 'Virqua/Sidie Hollow area map for Vernon Trails'),
+    ("{}/theme/img/map_gallery/CenturyRideMaps2015_draft3.png".format(SITEURL), 'Century Ride brochure map for Bike Natchitoches'),
+    ("{}/theme/img/map_gallery/Walmsley Graves 11x17.jpg".format(SITEURL), 'American Cemetery map just for fun'),
+    )
+    
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
